f1ea21be1254905a92192d89e1b906336dacc870
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-28 20:51:14 +0500

    Task2

--- Tyuiu.KruchininEP.Sprint5.Task2.V16.Test/DataServiceTest.cs
+++ Tyuiu.KruchininEP.Sprint5.Task2.V16.Test/DataServiceTest.cs
@@ -1,7 +1,7 @@
-﻿using Tyuiu.KruchininEP.Sprint5.Task1.V2.Lib;
+﻿using Tyuiu.KruchininEP.Sprint5.Task2.V16.Lib;
 
 
-namespace Tyuiu.KruchininEP.Sprint5.Task1.V2.Test
+namespace Tyuiu.BurdovKS.Sprint5.Task2.V16.Test
 {
     [TestClass]
     public class DataServiceTest
@@ -10,12 +10,11 @@ namespace Tyuiu.KruchininEP.Sprint5.Task1.V2.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint5\Tyuiu.KruchininEP.Sprint5.Task1.V2\bin\Debug\net8.0\OutPutFileTask1.txt";
+            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint5\Tyuiu.KruchininEP.Sprint5.Task2.V16\bin\Debug\net8.0\OutPutFileTask2.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
-
 
         }
     }
---
+++ Tyuiu.KruchininEP.Sprint5.Task2.V16/Program.cs
@@ -0,0 +1,80 @@
+﻿using Tyuiu.KruchininEP.Sprint5.Task2.V16.Lib;
+
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+        Console.Title = "Спринт #5 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        int[,] mass = new int[3, 3];
+
+        for (int i = 0; i < mass.GetLength(0); i++)
+        {
+            for (int j = 0; j < mass.GetLength(1); j++)
+            {
+                Console.Write($"Введите [{i},{j}] элемент массива: ");
+                mass[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < mass.GetLength(0); i++)
+        {
+            for (int j = 0; j < mass.GetLength(1); j++)
+            {
+                if (j == mass.GetLength(1) - 1) Console.Write(mass[i, j]);
+                else Console.Write(mass[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = ds.SaveToFileTextData(mass);
+
+        Console.WriteLine("Ответ записан в файл по пути:\n" + path);
+        Console.WriteLine("\nПолученные значения: ");
+
+        string[] massiv = File.ReadAllLines(path).ToArray();
+        int[,] mass2 = new int[massiv.Length, massiv[0].Split(";").Length];
+
+        for (int i = 0; i < massiv.Length; i++)
+        {
+            string[] temp = massiv[i].Split(";");
+            for (int j = 0; j < temp.Length; j++)
+                mass2[i, j] = Convert.ToInt32(temp[j]);
+        }
+
+        for (int i = 0; i < mass2.GetLength(0); i++)
+        {
+            for (int j = 0; j < mass2.GetLength(1); j++)
+            {
+                if (j == mass2.GetLength(1) - 1) Console.Write(mass2[i, j]);
+                else Console.Write(mass2[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.ReadLine();
+    }
+}
