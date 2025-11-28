d149f4538a31b1287e62d8cbdcab8f533f3f63df
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-28 21:06:45 +0500

    Task5

---
+++ Tyuiu.KruchininEP.Sprint5.Task5.V11.Test/DataService.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KruchininEP.Sprint5.Task5.V11.Lib;
+namespace Tyuiu.KruchininEP.Sprint5.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint5\Tyuiu.KruchininEP.Sprint5.Task5.V11\bin\Debug\net8.0\OutPutFileTask5.txt";
+            string tempPath = Path.GetTempPath();
+
+
+            string fileName = "OutOutFileTask0.txt";
+            string p = Path.Combine(tempPath, fileName);
+
+            FileInfo fileInfo = new FileInfo(path);
+
+
+            bool fileExists = fileInfo.Exists;
+
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
