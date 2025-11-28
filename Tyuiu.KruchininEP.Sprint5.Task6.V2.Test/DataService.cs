fa4dbca8ae570762e4c4e4d0524b5861177f87d1
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-28 21:17:06 +0500

    Task6

---
+++ Tyuiu.KruchininEP.Sprint5.Task6.V2.Test/DataService.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KruchininEP.Sprint5.Task6.V2.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint5.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint5\Tyuiu.KruchininEP.Sprint5.Task6.V2\bin\Debug\net8.0\OutPutFileTask6.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExist);
+        }
+
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\HASEE\source\repos\Tyuiu.KruchininEP.Sprint5\Tyuiu.KruchininEP.Sprint5.Task6.V2\bin\Debug\net8.0\OutPutFileTask6.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
